2ab92e747588b48f31d4028ebfe5a4c3f4980956
Nicholas Wayoe <nwayoe@yahoo.com>
2020-07-28 20:10:47 -0400

    created historyCell

--- FindMe/View Controllers/HistoryViewController.m
+++ FindMe/View Controllers/HistoryViewController.m
@@ -7,26 +7,28 @@
 //
 
 #import "HistoryViewController.h"
+#import "HistoryCell.h"
 
-@interface HistoryViewController ()
+@interface HistoryViewController () <UITableViewDelegate, UITableViewDataSource>
 
 @end
 
 @implementation HistoryViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath
+{
+    HistoryCell *cell = [tableView dequeueReusableCellWithIdentifier:@"HistoryCell"];
+    return cell;
 }
-*/
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 20;
+}
 
 @end
